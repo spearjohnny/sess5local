5557e05fac9f78555e26852e7272bf71b988467c
spearjohnny <spearkidume@gmail.com>
2021-05-27 18:49:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sess5local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
